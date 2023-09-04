db6cd6823a9c509bcfa49fc817f4d404e0b8616e
ZanGunawan <zunzee10@gmail.com>
2023-09-04 08:58:59 +0700

    ini web folio saya

---
+++ main.js
@@ -0,0 +1,19 @@
+// burger menu responsive
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const navList = document.querySelector("nav");
+
+    burger.addEventListener("click", () => {
+        navList.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+    });
+};
+
+navSlide()
+
+// clean form
+window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName("form")) {
+        form.reset();
+    }
+}
